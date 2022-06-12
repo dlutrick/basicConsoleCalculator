72574aa33ca298fc119ec762be3bd80bbbda6476
David <davidrlutrick@gmail.com>
2022-06-11 23:05:58 -0700

    added the four basic operators and their lambda expressions to calculate.java

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,2 +1,12 @@
 public class Calculate {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.registerOperation("+", Integer::sum);
+        calculator.registerOperation("-", (a, b) -> a - b);
+        calculator.registerOperation("*", (a, b) -> a * b);
+        calculator.registerOperation("/", (a, b) -> a / b);
+
+        int answer = calculator.calculate(360, "/", 9);
+        System.out.println(answer);
+    }
 }
